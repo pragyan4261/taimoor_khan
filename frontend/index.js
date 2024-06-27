69beb098995dd19ae15d1cce3d63df27133f9187
Kaushik-4 <kaushiktalukdar033@gmail.com>
2024-06-27 19:11:52 +0530

    project update

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('addAwardForm');
     const awardsTableBody = document.getElementById('awardsTableBody');
+    const project = document.getElementById('addProjectForm');
+    const projectTableBody = document.getElementById('table');
 
     // Fetch and display existing awards
     fetch('https://taimoor-khan.onrender.com/api/awards/read')
@@ -44,6 +46,43 @@ document.addEventListener('DOMContentLoaded', () => {
             `;
             awardsTableBody.appendChild(row);
             form.reset();
+            
+        })
+        .catch(error => console.error('Error:', error));
+    });
+    //handel project submission
+    project.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const newProject = {
+            investigator: project.investigator.value,
+            foreign_collaborator: project.name.value,
+            project_Title: project.project_title.value,
+            funding_Agency:project.founding_agency.value,
+            funds:project.funds.value,
+        };
+        console.log(newProject);
+
+        fetch('http://localhost:1335/projects/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newProject)
+        })
+        .then(response => response.json())
+        .then(data => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <th scope="row">${data.investigator}</th>
+                <td>${data.foreign_collaborator}</td>
+                <td>${data.project_Title}</td>
+                <td>${data.funding_Agency}</td>
+                <td>${data.funds}</td>
+
+            `;
+            projectTableBody.appendChild(row);
+            project.reset();
             
         })
         .catch(error => console.error('Error:', error));
